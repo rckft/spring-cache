f9d8d8b4b50b1b65eab6d6a7d533dbb8d75c3e40
M S <Maciek>
2025-05-16 14:00:11 +0200

    project init

---
+++ src/test/java/dev/rckft/cache/CacheApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rckft.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
